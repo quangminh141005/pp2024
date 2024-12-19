e0ea6b72271115a0d48fbea9e3ae56ea591f181e
quangminh1410 <minhnq.23bi14288@usth.edu.vn>
2024-12-20 00:19:33 +0700

    Labwork 2

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -5,13 +5,11 @@ class Person:
         self.dob = dob
 
     def __str__(self):
-        return f"ID: {self.id}, Name: {self.name}, DOB: {self.dob}"
-
+        return f"ID: {self.id}, Name: {self.name}, Dob: {self.dob}"
 
 class Student(Person):
     def __init__(self, id, name, dob):
         super().__init__(id, name, dob)
-
 
 class Course:
     def __init__(self, id, name):
@@ -19,118 +17,124 @@ class Course:
         self.name = name
 
     def __str__(self):
-        return f"Course ID: {self.id}, Name: {self.name}"
+        return f"Course {self.name}, ID: {self.id}"
 
 class Mark:
     def __init__(self):
         self.marks = {}
 
-    def addMark(self, course_id, student_id, mark):
+    def addMark(self, student_id, course_id, mark):
         if course_id not in self.marks:
             self.marks[course_id] = {}
         self.marks[course_id][student_id] = mark
 
-    def getMark(self, courseID):
-        if courseID in self.marks:
-            return {student.id: self.marks[courseID].get(student.id, "Not available")}
+    def getMark(self, course_id):
+        if course_id in self.marks:
+            return self.marks[course_id]
         return None
 
-class ManagementSystem:
+class Management:
     def __init__(self):
         self.students = []
         self.courses = []
         self.marks = Mark()
-    
+
     def inputStudent(self):
-        numStudents = int(input("Enter the number of student: "))
-        for i in range (numStudents):
-            id = input("Enter the student ID:").strip()
-            name = input("Enter the student name: ").strip()
-            dob = input("Enter the student date of birth: ").strip()
+        num_student = int(input("Enter the number of sudents: "))
+        for _ in range(num_student):
+            id = input("Enter the student ID: ")
+            name = input("Enter the student name: ")
+            dob = input("Enter the sudent date of birth: ")
             self.students.append(Student(id, name, dob))
 
     def inputCourse(self):
-        numCourses = int(input("Enter the number of course: "))
-        for i in range(numCourses):
-            id = input("Enter the number of course ID: ")
-            name = input("Enter the number of course name:") 
+        num_course = int(input("Enter the numbe of courses: "))
+        for _ in range(num_course):
+            id = input("Enter the ID for the course: ")
+            name = input("Enter the course name: ")
             self.courses.append(Course(id, name))
 
     def inputMark(self):
-        print("\nAvailable Course: ")
+        #Show the available course
+        print("\nAvailable course: ")
         for course in self.courses:
             print(course)
+        
+        #Check if the course is in the course list
+        chooseCourse = input("Enter the course ID to add mark: ")
+        checkCourse = any (chooseCourse ==  course.id for course in self.courses)
 
-        courseID = input("Enter the id of the course to input mark: ").strip()
-        courseFound = any(course.id == courseID for course in self.courses)
-
-        if courseFound:
+        #Add the mark to the course (condition: 0<=x<=20)
+        if checkCourse:
             for student in self.students:
                 while True:
                     try:
-                        mark = float(input(f"Enter the mark for student {student.name} (ID: {student.id}: )"))
+                        mark = int(input(f"Enter the mark for student {student.name} (ID: {student.id}): "))
                         if 0 <= mark <= 20:
                             break
                         else:
-                            print("Mark must be between 0 and 20")
+                            print("Mark must betweeen 0 and 20")
                     except ValueError:
                         print("Invalid input")
-                self.marks.addMark(courseID, student.id, mark)
+                self.marks.addMark(student.id, chooseCourse, mark)
         else:
-            print("Invalid cousre ID!")
+            print("Invalid course ID!!!!!")
 
-    def listStudents(self):
-        print("\nList of students:")
+    def listStudent(self):
+        print("\nList of all students: ")
         for student in self.students:
             print(student)
 
-    def listCourses(self):
-        print("\nList of courses: ")
+    def listCourse(self):
+        print("\nList of all course: ")
         for course in self.courses:
             print(course)
 
-    def listStudentMark(self):
-        self.listCourses()
-        course_id = input("Enter the course id to view mark: ").strip()
-        marks = self.marks.getMark(course_id)
+    def showMark(self):
+        self.listCourse()
+        # input couse
+        course_id = input("Enter the course to show mark: ").strip()
 
-        if marks is not None:
-            print(f"\nMark for course {course_id}: ")
-            for student in  self.students:
-                print(f"{student.name} (ID: {student.id}): {marks.get(student.id, "Not available")}")
+        # find course in mark
+        course_mark = self.marks.getMark(course_id)
+        if course_mark is not None:
+            print(f"\nMark for course {course_id}")
+            for student in self.students:
+                print(f"Student {student.name} (ID: {student.id}): {course_mark.get(student.id, 'Not available')}")
         else:
-            print("Invalid course ID")
+            print("Invalid course")
+
     def menu(self):
         while True:
-            print("\nMenu:")
-            print("1. Input students")
-            print("2. Input courses")
-            print("3. Input marks")
-            print("4. List students")
-            print("5. List courses")
-            print("6. List student marks")
+            print("\nMenu: ")
+            print("1. Input student.")
+            print("2. Input course.")
+            print("3. Input mark for one course.")
+            print("4. List all the students.")
+            print("5. List all the course.")
+            print("6. Show the mark for one course.")
             print("7. Exit")
 
-            choice = input("Choose an option: ").strip()
+            choose = input("Enter your choice: ")
 
-            if choice == "1":
+            if choose == "1":
                 self.inputStudent()
-            elif choice == "2":
+            elif choose == "2":
                 self.inputCourse()
-            elif choice == "3":
+            elif choose == "3":
                 self.inputMark()
-            elif choice == "4":
-                self.listStudents()
-            elif choice == "5":
-                self.listCourses()
-            elif choice == "6":
-                self.listStudentMark()
-            elif choice == "7":
-                print("Goodbye!")
+            elif choose == "4":
+                self.listStudent()
+            elif choose == "5":
+                self.listCourse()
+            elif choose == "6":
+                self.showMark()
+            elif choose == "7":
+                print("Au revoir!")
                 break
             else:
-                print("Invalid choice. Try again.")
-    
+                print("Input from 1 - 7, try again!")
+
 if __name__ == "__main__":
-    ms = ManagementSystem()
-    ms.menu()
+    mana = Management()
+    mana.menu()
