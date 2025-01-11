b89e70a74b9ced462be9a96572afa5851152d81b
quangminh1410 <minhnq.23bi14288@usth.edu.vn>
2025-01-11 09:57:07 +0700

    Fixed buggs and add decode()

--- 7.shell.py
+++ 7.shell.py
@@ -42,7 +42,7 @@ def process_input(command):
 def execute(command):
     parts = command.split()
     result = subprocess.run(parts, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print(result)
+    print(result.stdout.decode())
 
 def process_pipe(command):
     # Indentidy each process
@@ -54,14 +54,14 @@ def process_pipe(command):
             prev_result = subprocess.run(parts, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         else:
             result = subprocess.run(parts, stdin=prev_result, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print(result)
+    print(result.stdout.decode())
 
 def process(command):
     if '>' in command:
         process_output(command)
     elif '<' in command:
         process_input(command)
-    elif '|' in command(command):
+    elif '|' in command:
         process_pipe(command)
     else:
         execute(command)
@@ -70,13 +70,14 @@ def main():
     while True:
         try:
             command = input("Shell>> ")
-            if command.lower() in ['exit', 'quit']
+            if command.lower() in ['exit', 'quit']:
                 break
             process(command)
         except KeyboardInterrupt:
+            print()
             break
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
 
 
